27049e3292bc78c911b0306fb98a52b95e60f8d0
LUMINIȚA NEGRU <luminita.negru@stud.ubbcluj.ro>
2023-12-28 14:41:13 +0200

    Add project files.

---
+++ Project_Calories/Data/Project_CaloriesContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Project_Calories.Models;
+
+namespace Project_Calories.Data
+{
+    public class Project_CaloriesContext : DbContext
+    {
+        public Project_CaloriesContext (DbContextOptions<Project_CaloriesContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Project_Calories.Models.Food> Food { get; set; } = default!;
+    }
+}
---
+++ Project_Calories/Models/Food.cs
@@ -0,0 +1,12 @@
+﻿namespace Project_Calories.Models
+{
+    public class Food
+    {
+        public int FoodId { get; set; }
+        public string Name { get; set; }
+
+        public int Calories { get; set; }
+
+
+    }
+}
